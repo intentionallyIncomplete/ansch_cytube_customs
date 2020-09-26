8470585cb8e6acf993eaf51cac2ba47d0aca4abc
AlwaysRushing <alwaysinarush@protonmail.ch>
2020-09-26 00:26:27 -0700

    Adding GIF to left of playlist row
    Minor amount of adjusting done to fit new element, a GIF, onto the page. Placed to the left of the playlist row.

---
+++ custom_channel_layout.js
@@ -0,0 +1,12 @@
+/*
+* Custom styling behavior that adds and amends to the current layout.
+* Additions and behavior done by Quigly the Archivist with
+* approval from channel owner, Anschluss.
+*
+* Version 2020.09.26
+*/
+
+let lepGifDiv = $('<div class="col-lg-5 col-md-5" id="lep_gif"></div>');
+lepGifDiv.innerHTML = $('<img src="/media_content/leprechaun.gif"/>');
+
+$('#playlistrow').append(lepGifDiv);
